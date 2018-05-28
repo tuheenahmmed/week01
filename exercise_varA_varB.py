aa17b367235c80785f2a6d44fc7582ada762407c
tuheenahmmed <tuheenahmmed@icloud.com>
2018-05-28 12:12:44 +0900

    Add files via upload

--- exercise_varA_varB.py
+++ exercise_varA_varB.py
@@ -5,17 +5,22 @@ Created on Fri May 18 17:51:01 2018
 @author: etuhahm
 """
 
-varA=int(5)
-varB=int(6)
-type(varA)
-type(varB)
-
-if type(varA) or type(varB) == str:
-    print("string involved")
-elif varA > varB:
-    print ("bigger")
-elif varA == varB:
-    print ("equal")
-elif varA < varB:
-    print ("smaller")
     
+varA= 'a'
+varB= 'b'
+type (varA)
+type (varB)
+
+if type (varA) == str or type (varB)== str:
+    print ('string involved')
+    
+else:
+    
+  if varA > varB :
+          print ('bigger')
+          
+  if varA == varB:
+          print ('equal')
+          
+  if varA < varB:
+          print ('smaller')
